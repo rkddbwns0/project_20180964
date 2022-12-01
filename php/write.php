e0df28157c94504e48b2b2fc779a3603f4b6eb74
rkddbwns0 <112998995+rkddbwns0@users.noreply.github.com>
2022-12-01 17:44:18 +0900

    Update write.php

--- php/write.php
+++ php/write.php
@@ -69,11 +69,6 @@
                             <td>내용</td>
                             <td><textarea name="content" cols=75 rows=15></textarea></td>
                         </tr>
-
-                        <tr>
-                            <td>비밀번호</td>
-                            <td><input type="password" name="pw" size=15 maxlength=15></td>
-                        </tr>
                     </table>
 
                     <center>
